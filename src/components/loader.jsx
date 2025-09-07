c5debf402ba6584249cfda72a0452a06232eb30a
itsLohit <109834876+itsLohit@users.noreply.github.com>
2025-09-08 00:58:33 +0530

    feat: add loading icon

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,7 +1,11 @@
+import '../styles/loader.css';
+
 export default function Loader() {
   return (
-    <div className="loader">
-      <p>Preparing the Valley of Peace...</p>
+    <div className="loader-container">
+      <p>Preparing the Valley of Peace</p>
+      <span className="loader"></span>
+      
     </div>
   );
 }
